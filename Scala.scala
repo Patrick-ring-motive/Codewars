a314e3c704138d43fd2be431770664d3d5cf7da1
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-05-30 14:07:01 -0500

    Update Scala.scala

---
+++ Scala.scala
@@ -0,0 +1,19 @@
+object Static: 
+    val engine=new javax.script.ScriptEngineManager().getEngineByName("nashorn");
+def solveRunes(runes: String): Int = {   
+        return Static.engine.eval("""
+        
+String.prototype.includes=function(x){return this.indexOf(x)>-1;};
+function solveExpression(exp) {
+  exp = exp.replace('=','==').replace('--','- -');
+  for(var i=0;i<10;i++){
+   if(i==0&&(/([^0-9?]|^)[?][0-9?]/.test(exp)))continue;
+   if(!exp.includes(i)&&eval(exp.replace(/[?]/g,i)))return i;
+  }
+  return -1;
+}
+solveExpression('"""+runes+"""');
+
+
+    """).toString().toInt;
+}
