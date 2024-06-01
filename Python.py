20d33723c4d8dd5ebb2b4af491e798de79d62ca7
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-05-31 19:17:56 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,6 +1,6 @@
 import subprocess,sys;subprocess.check_call([sys.executable,"-Wignore","-u","-m","pip","-q","--disable-pip-version-check","install","--no-clean","js2py"]);import js2py as engine
 def solution(string):
-    return js2py.eval_js("""
+    return engine.eval_js("""
     
     function solution(str){
       return str.split('').reverse().join('');
