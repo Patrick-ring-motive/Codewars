2b30c3b02375aac04b99b775bb547fe52a4b9fe3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-06-05 15:10:35 -0500

    Update GavaScript.groovy

--- GavaScript.groovy
+++ GavaScript.groovy
@@ -1,11 +1,30 @@
-class Kata {
-    static final def engine=new javax.script.ScriptEngineManager().getEngineByName("groovy");
-    static final noSpace(String str) {
+class EvenOdd {
+
+    static final engine=new javax.script.ScriptEngineManager().getEngineByName("groovy");
+
+    static final evenOrOdd(number){
+
         return engine.eval("""
-        
-        function noSpace(String str){
-          return str.replaceAll(' ','')
-        }
-        noSpace('${str}');
-        
-    """.replaceAll(/(^|[^a-zA-Z0-9])function/,'$1def')).toString();}}
+
+       
+
+      function EvenOdd(const number){
+
+        return number%2?'Odd':'Even';
+
+      }
+
+      EvenOdd(${number});
+
+       
+
+    """.replaceAll(/(^|[^a-zA-Z0-9])function ([ ]*[a-zA-Z])/,'$1def $2')
+       .replaceAll(/(^|[^a-zA-Z0-9])var /,'$1static def ')
+       .replaceAll(/(^|[^a-zA-Z0-9])let /,'$1def ')
+       .replaceAll(/(^|[^a-zA-Z0-9])const /,'$1final def ')
+      ).toString();
+
+    }
+
+}
+
