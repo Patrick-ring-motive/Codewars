ce34ccba77c868964cff3b1102fd9422ebf43ca0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-06-05 18:02:20 -0500

    Update GavaScript.groovy

--- GavaScript.groovy
+++ GavaScript.groovy
@@ -1,22 +1,14 @@
 class EvenOdd {
-
     static final engine=new javax.script.ScriptEngineManager().getEngineByName("groovy");
-
     static final evenOrOdd(number){
-
         return engine.eval("""
-
-       
 
       function EvenOdd(const number){
 
         return number%2?'Odd':'Even';
 
       }
-
       EvenOdd(${number});
-
-       
 
     """.replaceAll(/(^|[^a-zA-Z0-9])function\s+([a-zA-Z_]\w*)\s*\(/,'$1def $2(')
        .replaceAll(/(^|[^a-zA-Z0-9])function\s+\(/,'$1def lambda'+System.nanoTime()+'(')
@@ -24,9 +16,5 @@ class EvenOdd {
        .replaceAll(/(^|[^a-zA-Z0-9])let\s/,'$1def ')
        .replaceAll(/(^|[^a-zA-Z0-9])const\s/,'$1final def ')
        .replaceAll(/=>/,'->')
-      ).toString();
-
-    }
-
-}
+      ).toString();}}
 
