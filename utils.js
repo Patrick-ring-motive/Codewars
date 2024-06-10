005e9bb4980e38b56196b99ca79c7db8e551a31c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-06-09 20:41:51 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,5 +2,5 @@
 try{
 eval("Math.sum=function(){try{if(arguments.length==1){try{return[...arguments[0]].reduce((x,y)=>+x + +y,0);}catch(e){}}return [...arguments].reduce((x,y)=>+x + +y,0);}catch(e){return 0}};")
 }catch(e){
-Math.sum=function(){try{if(arguments.length==1){try{var s=arguments[0];if(typeof s=='string'){s=s.split('');}return [].reduce.call(s,function(x,y){return 1*x + 1*y},0);}catch(e){}}var ss=arguments;try{ss==Array.from(ss);}catch(e){}return [].reduce.call(function(x,y){return 1*x + 1*y,0);}catch(e){return 0}};
+Math.sum=function(){try{if(arguments.length==1){try{var s=arguments[0];if(typeof s=='string'){s=s.split('');}return [].reduce.call(s,function(x,y){return 1*x + 1*y},0);}catch(e){}}var ss=arguments;try{ss=Array.from(ss);}catch(e){}return [].reduce.call(function(x,y){return 1*x + 1*y},0);}catch(e){return 0}};
 }
