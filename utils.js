65bb4d50db835db737e8a76903121d6dad944016
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-06-09 21:47:16 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,3 +4,6 @@ eval("Math.sum=function(){try{if(arguments.length==1){try{return[...arguments[0]
 }catch(e){
 Math.sum=function(){try{if(arguments.length==1){try{var s=arguments[0];if(typeof s=='string'){s=s.split('');}return [].reduce.call(s,function(x,y){return 1*x + 1*y},0);}catch(e){}}var ss=arguments;try{ss=Array.from(ss);}catch(e){}return [].reduce.call(function(x,y){return 1*x + 1*y},0);}catch(e){return 0}};
 }
+
+global.copy=function(x){return JSON.parse(JSON.stringify(x));}
+
