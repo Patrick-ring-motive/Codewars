58fd1be6cd259328733309eacfe65bf32f6eaf7c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-07-16 15:45:06 -0500

    Update quickmafs.ts

--- quickmafs.ts
+++ quickmafs.ts
@@ -1,4 +1,1 @@
-let oN=x=>Object.getOwnPropertyNames(x);let aP=Array.prototype,sP=String.prototype,nP=Number.prototype,rP=RegExp.prototype;oN(aP).map(x=>sP[x]??=function(){const y=[...this][x](...arguments);return y?.join('')??y});oN(rP).map(x=>sP[x]??=function(re){return re[x](this);});oN(sP).map(x=>nP[x]??=function(){const y=`${this}`[x](...arguments);return isNaN(y)?y:Number(y);});
-  
-
-
+let oN=x=>Object.getOwnPropertyNames(x);let aP=Array.prototype,sP=String.prototype,nP=Number.prototype,rP=RegExp.prototype;oN(aP).map(x=>sP[x]??=function(){const y=[...this][x](...arguments);return y?.join('')??y});oN(rP).map(x=>sP[x]??=function(re){return re[x](this);});oN(sP).map(x=>nP[x]??=function(){const y=`${this}`[x](...arguments);return isNaN(y)?y:Number(y);});oN(Math).map(x=>global[x]=function(){return Math[x](...arguments);});
