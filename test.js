3e6a394945312e721a37266d7f2b055433d3fbb5
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-07-19 17:48:35 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
+console.log(require('child_process').execSync('ls').toString());
+
 const fs = require('fs');
+
+try{
 const data = fs.readFileSync('./test.js',
     { encoding: 'utf8', flag: 'r' });
 console.log(data);
+}catch(e){
+const data = fs.readFileSync('./index.js',
+    { encoding: 'utf8', flag: 'r' });
+console.log(data);
+    
+}
