3b4744978c86d3229fee2117a2eab195a52c57f0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-07-20 08:06:22 -0500

    Update prime.js

--- prime.js
+++ prime.js
@@ -3,12 +3,12 @@ Object.freeze(Math);
 Object.freeze(Math.round);
 const sqrt5 = 5**.5;Object.freeze(sqrt5);
 const phi = (1+sqrt5)/2;Object.freeze(phi);
-const 1_phi = 1-phi;Object.freeze(1_phi);
+const $1_phi = 1-phi;Object.freeze($1_phi);
 
 const fib=(n)=>{
  const x=n;
- return Math.round((phi**x - 1_phi**x)/sqrt5);
-}Object.freeze(fib);
+ return Math.round((phi**x - $1_phi**x)/sqrt5);
+};Object.freeze(fib);
 
 const isPrime=(n)=>{
   const x=n;
@@ -19,4 +19,4 @@ const isPrime=(n)=>{
     if(x%i===0)return false;
   }
   return true;
-}Object.freeze(isPrime);
+};Object.freeze(isPrime);
