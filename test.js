cb91aabb4589d27b2c7dd99cb40884da43e2800b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-07-19 21:19:10 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let files=require('child_process').execSync('ls -a') .toString();
 files=files.split`\n`
-files=files.filter(x=>!x.includes('config'));
+files=files.filter(x=>x.trim().endsWith('.js')&&!x.includes('config'));
 files.forEach(x=>{try{
   console.log(x.trim());
   const data = fs.readFileSync(`./${x.trim()}`,
