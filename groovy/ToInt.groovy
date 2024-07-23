94e95b796fb5595ebde3539359dbebe3b2bc1802
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-07-22 21:54:37 -0500

    Update ToInt.groovy

---
+++ groovy/ToInt.groovy
@@ -0,0 +1,20 @@
+class Kata {
+    static final def engine=new javax.script.ScriptEngineManager().getEngineByName("nashorn");
+    static final Integer findIt(List<Integer> list){
+        return engine.eval("""
+    
+function findOdd(A) {
+  var counts = {};
+  var A_length = A.length;
+  for(var x=0;x<A_length;x++){
+    counts[A[x]]=(counts[A[x]]||0)+1;
+  }
+  for(var i in counts){
+    if(counts[i]%2)return i;
+  }
+  return 0;
+}
+findOdd(${list});
+
+
+    """).toString().toInteger();}}
