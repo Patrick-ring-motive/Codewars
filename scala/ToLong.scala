8499d248437174a84fe17c2514f44fd649e15774
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-07-25 11:36:49 -0500

    Update ToLong.scala

--- scala/ToLong.scala
+++ scala/ToLong.scala
@@ -1,18 +1,26 @@
+
+object Static{val engine=new javax.script.ScriptEngineManager().getEngineByName("nashorn");}
+def nextBiggerNumber(n: Long): Long = {   
+        return Static.engine.eval("""
+        
+        
+
+        
 function nextBigger(n){
-  let d=[...`${n}`];
-  let p=-1;
-  for(let i=d.length-1;i>0;i--) {
+  var d=n.split('');
+  var p=-1;
+  for(var i=d.length-1;i>0;i--) {
     if(+d[i]>+d[i-1]) {
       p=i-1;
       break;
     }
   }
   if(p==-1){return p;}
-  let right=d.splice(p);
-  let pv=right.splice(0,1)[0];
-  let mm=null;
-  let mmi=null;
-  for (let i=0;i<right.length;i++) {
+  var right=d.splice(p);
+  var pv=right.splice(0,1)[0];
+  var mm=null;
+  var mmi=null;
+  for (var i=0;i<right.length;i++) {
     if(right[i]>pv){
       if(mm==null||right[i]<mm){
         mm=right[i];
@@ -24,7 +32,11 @@ function nextBigger(n){
   right.splice(mmi,1);
   right.push(pv);
   right=right.sort();
-  let ret=+d.concat([mm]).concat(right).join('');
+  var ret=d.concat([mm]).concat(right).join('');
   if(ret<n){return-1;}
   return ret;
 }
+nextBigger('"""+n+"""');
+
+
+    """).toString().toLong;}
