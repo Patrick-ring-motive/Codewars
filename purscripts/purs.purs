69579da4b7779241864d84a3258f82fe7de2f70e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-04 22:24:36 -0500

    Update purs.purs

--- purscripts/purs.purs
+++ purscripts/purs.purs
@@ -1,4 +1,4 @@
-module LeapYears (isLeapYear) where
+module ROT13 (rot13) where
 
 import Prelude
 import Effect (Effect)
@@ -9,16 +9,18 @@ import Effect.Unsafe (unsafePerformEffect)
 funct :: Int -> Int
 funct x = x
 
-getFunctionConstructor :: forall r. Record r -> Effect (forall a b. a -> b)
-getFunctionConstructor obj = unsafeCoerce $ unsafeGet "constructor" obj
-
-isLeapYear :: Int -> Boolean
-isLeapYear year = do
-  let cnstr = getFunctionConstructor {constructor: unsafeCoerce funct}
-  let constructor = unsafeCoerce unsafeGet "constructor" cnstr
+rot13 :: String -> String
+rot13 str = do
+  let constructor = unsafeCoerce unsafeGet "constructor" funct
   let eval = unsafeCoerce unsafePerformEffect $ constructor "return eval"
-  let script = "()=>!!((!(" <> unsafeCoerce year <>"%4))&&((" <> unsafeCoerce year <>"%100)||(!(" <> unsafeCoerce year <>"%400))))"
+  let script = "\
+\  ()=>{\
+\    let a = 'abcdefghijklmnopqrstuvwxyz';\
+\    let A = a.toUpperCase();\
+\    let c = 'nopqrstuvwxyzabcdefghijklm';\
+\    let C = c.toUpperCase();\
+\    let m =(''+(()=>{/*" <> unsafeCoerce str <> "*/})).slice(7,-3);\
+\    return [...m].map(x=>a.includes(x)?c[a.indexOf(x)]:C.includes(x)?C[A.indexOf(x)]:x).join('');\
+\  }"
   let result = unsafeCoerce unsafePerformEffect $ eval script
-  if result
-  then true
-  else false
+  result
