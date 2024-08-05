10fad8bbe4d668cad72c3805fa59a74916c7180a
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-05 13:26:56 -0500

    Update codewars.js

--- js/codewars.js
+++ js/codewars.js
@@ -1,4 +1,10 @@
 void async function main(){
-global.declare??await import('https://unpkg.com/javaxscript@1.1.78/framework.js');
+globalThis.declare??await import('https://unpkg.com/javaxscript@1.1.78/framework.js');
 
+declare(()=>{
+  queryApplyAll('script[src^="https://www.codewars.com"]',el=>{
+    el.src=el.src.replace("https://www.codewars.com",location.origin);
+  });
+});
+  
 }();
