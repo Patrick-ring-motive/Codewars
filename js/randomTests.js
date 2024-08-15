dde2704d3c3cc1066df43e73eab9a1ca45c0b342
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-15 18:50:53 -0500

    Update randomTests.js

--- js/randomTests.js
+++ js/randomTests.js
@@ -12,9 +12,9 @@
   const copyIter = (iter) => { 
       const arr = [...iter];
       const i = arr.values();
-      objDefProp(iter,"next",() => i.next.call(i));
-      objDefProp(iter,Symbol.iterator,() => i);
-      objDefProp(iter,"valueOf",() => i);
+      objDefProp(iter,"next",function next(){ return i.next.call(i,...arguments);});
+      objDefProp(iter,Symbol.iterator,function iterator(){ return i;});
+      objDefProp(iter,"valueOf",function valueOf(){return i;});
       return [...arr].values();
   }
   
